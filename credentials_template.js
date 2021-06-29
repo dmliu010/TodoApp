7688d677bee811d2a564630d9472dae95bdfbd64
dmliu010 <luckydliu010@gmail.com>
2021-06-28 17:02:25 -0700

    Add template for credentials

---
+++ credentials_template.js
@@ -0,0 +1,8 @@
+let mongoCred = //Mongo Credentials Go Here
+let siteCred = 'Basic YWRtaW46cGFzc3dvcmQ=' //Site Base64 Password goes here. Default is admin:password
+
+module.exports = {
+    mongoCred: mongoCred,
+    siteCred: siteCred
+
+}
